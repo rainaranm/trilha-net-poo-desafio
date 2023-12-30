4f3ac7b1f37bbdb0c39c29b6e7bf40cb7bf616af
rainara <rainara.n.moura@gmail.com>
2023-12-30 18:13:34 -0300

    Desafio concuído

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,24 +3,45 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo;
+        private string IMEI;
+        private int Memoria;
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+
         }
 
-        public void Ligar()
+        public void Ligar(string numero)
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"Ligando para {numero}");
         }
 
-        public void ReceberLigacao()
+        public void ReceberLigacao(string numero)
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine($"Recebendo ligação de {numero}");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
+
+        public void Especificacoes(){
+            Console.WriteLine("\nEspecificações:");
+            Console.WriteLine($"- Número atual: {Numero}");
+            Console.WriteLine($"- Modelo: {Modelo}");
+            Console.WriteLine($"- IMEI: {IMEI}");
+            Console.WriteLine($"- Amarzenamento: {Memoria} GB");
+        }
+
+        public void MudarNumero(string numeroNovo)
+        {
+            Numero = numeroNovo;
+            Console.WriteLine($"Número mudado para {Numero}");
+        }
+        
     }
+
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone nokia = new Nokia(numero: "00 91234-5678", modelo:"AB1234-5", imei:"000000000000000", memoria: 32);
+Smartphone iphone = new Iphone(numero: "00 9876-54321", modelo: "CD6789-0", imei:"111111111111111", memoria: 64);
+
+Console.WriteLine("\n** TESTE NOKIA: **\n");
+nokia.Ligar(iphone.Numero);
+nokia.InstalarAplicativo("Instagram");
+nokia.Especificacoes();
+
+Console.WriteLine("\n** TESTE IPHONE: **\n");
+iphone.ReceberLigacao(nokia.Numero);
+iphone.InstalarAplicativo("Facebook");
+iphone.Especificacoes();
+
+Console.WriteLine("\n** Atualizando número de telefone: **");
+iphone.MudarNumero("22 9098-7234");
+iphone.Especificacoes();
+Console.WriteLine();
+
+
+
+
